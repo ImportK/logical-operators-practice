8b1e182aae6466a9e13f447d45448fe4505b0510
Kaye Janine Remigio <remigiokayejanine@gmail.com>
2024-09-10 23:26:05 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+let favNum1 = 8;
+let favNum2 = 18;
+
+// Greater than
+console.log("Greater than:" , favNum1 > favNum2);
+console.log("Greater than:" , favNum2 > favNum1);
+
+// Less than
+console.log("Less than:" , favNum1 < favNum2);
+console.log("Less than:" , favNum2 <= favNum1);
+
+// Greater than or equal to
+console.log("Greater than is equal to:" , favNum1 >= favNum2);
+console.log("Greater than is equal to:" , favNum2 >= favNum1);
+
+// Less than or equal to
+console.log("Less than or equal to:" , favNum1 <=favNum2);
+console.log("Less than or equal to:" , favNum2 <= favNum1);
+
+// Strict equality
+console.log("Strict equality:" , favNum1 === favNum2);
+
+
+// Loose equality
+console.log("Loose equality:" , favNum1 == favNum2);
+
+// Strict inequality
+console.log("Strict inequality:" , favNum1 !== favNum2);
+
+
+// Loose inequality
+console.log("Loose inequality:" , favNum1 != favNum2);
+
+
+// AND
+console.log("AND:" , favNum1 < favNum2 && favNum2 > favNum1);
+console.log("AND:" , favNum1 < favNum2 && favNum2 < favNum1);
+
+// OR
+console.log("OR:" , favNum1 > favNum2 || favNum2 > favNum1);
+
+// NOT
+console.log("NOT:" , !favNum1 <  favNum2);
+console.log("NOT:" , !favNum1 > favNum2);
